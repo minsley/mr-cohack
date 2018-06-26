27fc2e2ae5e8a7b4c251e93035fe5fe45a5688e0
Matt Insley <insley.matthew@gmail.com>
2018-06-25 23:49:43 -0700

    Chart working on hololens

---
+++ Assets/_scripts/PopulateDummyChartData.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopulateDummyChartData : MonoBehaviour 
+{
+	public float UpdatePeriod = 0.2f; // Update chart ever X seconds
+	public float RangeMin = 0f;
+	public float RangeMax = 10f;
+
+	private DD_DataDiagram diagram;
+	private List<GameObject> lines;
+	private float timeSinceUpdate = 0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		diagram = GetComponent<DD_DataDiagram>();
+
+		lines = new List<GameObject>();
+		lines.Add(diagram.AddLine("Device 001", Color.blue));
+		lines.Add(diagram.AddLine("Device 002", Color.magenta));
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		timeSinceUpdate += Time.deltaTime;
+
+		if(timeSinceUpdate >= UpdatePeriod)
+		{
+			timeSinceUpdate = 0f;
+			UpdateChart();
+		}
+	}
+
+	void UpdateChart()
+	{
+		foreach(var line in lines)
+		{
+			diagram.InputPoint(line, new Vector2(1, Random.Range(RangeMin, RangeMax)));
+		}
+	}
+}
